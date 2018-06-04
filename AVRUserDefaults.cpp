a4bfcd177859fffdbc30aaceb0a12e4075c0052d
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2018-06-04 08:18:34 +0100

    implemented a simple library dependant on <avr/eeprom.h> for storing and reading in the atmega's internal EEPROM

---
+++ AVRUserDefaults.cpp
@@ -0,0 +1,26 @@
+//
+//  AVRUserDefaults.cpp
+//  AVRSleep
+//
+//  Created by Stoyan Stoyanov on 3.06.18.
+//  Copyright © 2018 Stoyan Stoyanov. All rights reserved.
+//
+
+#include "AVRUserDefaults.h"
+
+#pragma mark - Bluetooth Configured
+
+bool AVRUserDefaults::isBluetoothAlreadyConfigured() {
+    byte storage;
+    eeprom_read_block((void*)&storage, (void*)0, sizeof(storage)); // read container, address, length of the read 
+    return storage == 0;
+}
+
+void AVRUserDefaults::setIsBluetoothAlreadyConfigured(bool isConfigured) {
+
+    if (isBluetoothAlreadyConfigured() == isConfigured) { return; }
+    
+    byte record = isConfigured ? 0x00 : 0xFF;
+    eeprom_write_block((const void*)&record, (void*)0, sizeof(record)); // value to be written, address, length of the write
+}
+
